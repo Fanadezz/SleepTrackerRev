3b871005a60eb5f1f0b65f224214f32b394ca2b7
Antony <vontonnie@gmail.com>
2020-12-22 20:03:42 +0300

    Add custom ViewHolder - TextItemViewHolder

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,5 +1,14 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.TextItemViewHolder
 
-class SleepNightAdapter : RecyclerView.Adapter<>(){}
+
+
+
+     class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>(){
+
+
+
+}
