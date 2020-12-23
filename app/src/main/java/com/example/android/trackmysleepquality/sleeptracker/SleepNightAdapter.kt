74f3c66178b6962d90b7ab76dc8b8051d5b6f445
Antony <vontonnie@gmail.com>
2020-12-23 07:36:37 +0300

    Add custom text_item_view

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.TextItemViewHolder
 import com.example.android.trackmysleepquality.database.SleepNight
 
@@ -14,14 +16,24 @@ var data = mutableListOf<SleepNight>()
 
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
 
-        val elementAtPosition = data[position]
-        holder.textView.text = elementAtPosition.sleepQuality.toString()
+        val item = data[position]
+        holder.textView.text = item.sleepQuality.toString()
 
 
 
     }
 
+
+    //tells recyclerView how to draw
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        TODO("Not yet implemented")
+
+
+        //get layout inflater from parent view which knows much about themselves
+        val inflater = LayoutInflater.from(parent.context)
+
+        //inflate layout
+
+        inflater.inflate(R.layout.)
+
     }
 }
