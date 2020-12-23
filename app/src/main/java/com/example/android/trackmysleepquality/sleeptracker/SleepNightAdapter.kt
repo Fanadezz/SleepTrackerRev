18372c0ccdb81e2ba7fc866b73baad953ae7c91c
Antony <vontonnie@gmail.com>
2020-12-23 07:30:07 +0300

    Implement onBindViewHolder

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,14 +1,27 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
+
+
+class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>(){
+var data = mutableListOf<SleepNight>()
+
+         override fun getItemCount(): Int  = data.size
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+
+        val elementAtPosition = data[position]
+        holder.textView.text = elementAtPosition.sleepQuality.toString()
 
 
 
+    }
 
-     class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>(){
-
-
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        TODO("Not yet implemented")
+    }
 }
