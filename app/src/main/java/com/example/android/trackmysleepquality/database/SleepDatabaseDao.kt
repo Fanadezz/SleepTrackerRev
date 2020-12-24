0075da86b5e448cc6770a7e3d1be36676c9b2c0b
Antony <vontonnie@gmail.com>
2020-12-24 17:19:45 +0300

    Update SleepDatabaseDAO with the missing 2 fxns

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -52,12 +52,14 @@ interface SleepDatabaseDao {
 
     //get all night and return a LiveData list
 
-    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
-    fun getAllNights(): LiveData<List<SleepNight>>
-
-    //nullable because at the beginning there is no night and when clear() is called
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
-    fun getTonight(): SleepNight?
+    suspend fun getTonight(): SleepNight?
+
+    /**
+     * Selects and returns the night with given nightId.
+     */
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    fun getNightWithId(key: Long): LiveData<SleepNight>
 
 
 
